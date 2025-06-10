5cdad8ad6a632a8fdb1d55a0abbebe200ee18758
ziiamodel <guno16530@gmail.com>
2025-06-10 07:56:45 +0530

    Update and rename get_posts.py to save-posts.js

--- my/api/get_posts.py
+++
@@ -1,9 +0,0 @@
-import json
-
-def handler(req, res):
-    try:
-        with open("posts.json", "r") as f:
-            data = json.load(f)
-        return res.json(data)
-    except Exception as e:
-        return res.status(500).send(str(e))
