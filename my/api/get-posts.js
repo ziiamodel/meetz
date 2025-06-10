5a6bf60410a8e0f9c087a05b284b529f3eaabd84
ziiamodel <guno16530@gmail.com>
2025-06-10 09:34:50 +0530

    Delete my/api directory

--- my/api/get-posts.js
+++
@@ -1,9 +0,0 @@
-// api/get-posts.js
-const fs = require('fs');
-const path = require('path');
-
-module.exports = async (req, res) => {
-  const jsonPath = path.join(process.cwd(), 'posts.json');
-  const data = fs.readFileSync(jsonPath);
-  res.status(200).json(JSON.parse(data));
-};
--- my/api/save-posts.js
+++
@@ -1,9 +0,0 @@
-// api/save-posts.js
-const fs = require('fs');
-const path = require('path');
-
-module.exports = async (req, res) => {
-  const jsonPath = path.join(process.cwd(), 'posts.json');
-  fs.writeFileSync(jsonPath, JSON.stringify(req.body, null, 2));
-  res.status(200).json({ success: true });
-};
